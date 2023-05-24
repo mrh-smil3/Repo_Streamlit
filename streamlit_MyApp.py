fbd8f343b39e060613802c819085f3d012adeaf1
ARIF <50254637+mrh-smil3@users.noreply.github.com>
2023-05-24 15:30:48 +0700

    Update streamlit_MyApp.py

--- streamlit_MyApp.py
+++ streamlit_MyApp.py
@@ -1,36 +1,48 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+def main():
+    # Judul
+    st.title("Portofolio Saya")
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+    # Deskripsi
+    st.write("""
+    Selamat datang di portofolio saya! Saya adalah seorang pengembang perangkat lunak dengan minat utama dalam pembangunan web dan kecerdasan buatan.
+    """)
+    
+    # Informasi Pribadi
+    st.header("Informasi Pribadi")
+    st.subheader("Nama")
+    st.write("John Doe")
+    st.subheader("Email")
+    st.write("johndoe@example.com")
+    st.subheader("Lokasi")
+    st.write("Jakarta, Indonesia")
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    # Pendidikan
+    st.header("Pendidikan")
+    st.subheader("Sarjana Teknik Informatika")
+    st.write("Universitas ABC (2015-2019)")
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+    # Pengalaman Kerja
+    st.header("Pengalaman Kerja")
+    st.subheader("Pengembang Perangkat Lunak")
+    st.write("Perusahaan XYZ (2019-2021)")
+    st.write("- Mengembangkan aplikasi web menggunakan Python dan Flask.")
+    st.write("- Memelihara basis data dan menerapkan perubahan sesuai kebutuhan.")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    # Proyek
+    st.header("Proyek")
+    st.subheader("Aplikasi Pencarian Gambar")
+    st.write("- Mengembangkan aplikasi pencarian gambar menggunakan Python dan Django.")
+    st.write("- Menggunakan API Pexels untuk mendapatkan gambar.")
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    st.subheader("Sistem Rekomendasi Film")
+    st.write("- Mengembangkan sistem rekomendasi film menggunakan Python dan Scikit-learn.")
+    st.write("- Menggunakan dataset IMDb untuk membangun model rekomendasi.")
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    # Footer
+    st.write("---")
+    st.write("Terima kasih telah mengunjungi portofolio saya.")
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if __name__ == '__main__':
+    main()
